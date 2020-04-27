7a1b01d6d66664cee5dc682911bba7b42ff1276c
Sergio Di Fiore <difiores@outlook.com>
2020-04-27 16:02:37 -0300

    Chain binding

---
+++ BethanysPieShopHRM.Server/Components/DateFieldBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Server.Components
+{
+    public class DateFieldBase : ComponentBase
+    {
+        
+    }
+}
